e35092791ce9872e6fda2533d2116e4ea91b3bfb
Sharad Mathukiya <saradmathukiya9@gmail.com>
2025-05-20 09:16:45 +0530

    Update API URL to Render backend

--- frontend/src/services/aiService.js
+++ frontend/src/services/aiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://notes-backend-aq02.onrender.com/api";
 
 export const summarizeNote = async (content) => {
   try {
